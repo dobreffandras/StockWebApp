64299a9c5de8932b45fb3c8526117362932ef8e0
András Dobreff <dobreffandras@gmail.com>
2022-07-22 10:31:02 +0200

    Change homepage to have menu

--- stock-web-app/src/App.tsx
+++ stock-web-app/src/App.tsx
@@ -5,17 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='App-menuItem'>
+          Home
+        </div>
+        <div className='App-menuItem'>
+          Stocks
+        </div>
       </header>
     </div>
   );
