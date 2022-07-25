76f905802d1435a7bc197c69cb0bccdd5e2fed3c
András Dobreff <dobreffandras@gmail.com>
2022-07-25 13:56:05 +0200

    Add StocksController

---
+++ StockWebApp/Controllers/StocksController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockWebApp.Dtos;
+
+namespace StockWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StocksController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<BasicStock> GetWithBaseInfo()
+        {
+            return new[]
+            {
+                new BasicStock(
+                    new Company("AAPL", "NASDAQ", "Apple Inc."),
+                    150.6,
+                    "USD",
+                    -1.26,
+                    0.81),
+                new BasicStock(
+                    new Company("GOOG", "NASDAQ", "Alphabet Inc Class C"),
+                    108.36,
+                    "USD",
+                    -6.68,
+                    5.81),
+                new BasicStock(
+                    new Company("NFLX", "NASDAQ", "Netflix"),
+                    220.44,
+                    "USD",
+                    -3.44,
+                    1.54)
+            };
+        }
+    }
+}
---
+++ StockWebApp/Dtos/BasicStock.cs
@@ -0,0 +1,9 @@
+﻿namespace StockWebApp.Dtos
+{
+    public record BasicStock(
+        Company Company,
+        double StockPrice,
+        string Currency,
+        double ChangePoint,
+        double Changepercent);
+}
