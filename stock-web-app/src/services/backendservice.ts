680cfbee741aba35bbbc22a99bf12c068cbc9615
András Dobreff <dobreffandras@gmail.com>
2022-07-26 10:56:06 +0200

    Prepare frontend to call backend stock by symbol

--- stock-web-app/src/services/backendservice.ts
+++ stock-web-app/src/services/backendservice.ts
@@ -1,8 +1,18 @@
 import { BasicStock } from "../types/types";
+const host = process.env.REACT_APP_BACKEND_HOST;
+
+
+function fetchJson<T>(url: string) : Promise<T> {
+    return fetch(url).then(res => res.json());
+}
 
 class Backendservice {
-    async fetchStocks() : Promise<BasicStock[]> {
-        return await fetch(`${process.env.REACT_APP_BACKEND_HOST}/stocks`).then(res => res.json())
+    fetchStocks() : Promise<BasicStock[]> {
+        return fetchJson<BasicStock[]>(`${host}/stocks`);
+    }
+
+    fetchStock(symbol: string) : Promise<BasicStock> {
+        return fetchJson<BasicStock>(`${host}/stocks/${symbol}`);
     }
 }
 
