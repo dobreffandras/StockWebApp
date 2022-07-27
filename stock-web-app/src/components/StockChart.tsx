45438fe6ebee9f329eab3e0e5012634d6d5f5f6c
András Dobreff <dobreffandras@gmail.com>
2022-07-27 13:57:09 +0200

    Extract datapoints to react state

--- stock-web-app/src/components/StockChart.tsx
+++ stock-web-app/src/components/StockChart.tsx
@@ -1,38 +1,45 @@
 import 'chart.js/auto';
 import 'chartjs-adapter-moment';
-import { ChartData } from 'chart.js/auto';
+import { ChartData, ChartDataset, ChartDatasetProperties } from 'chart.js/auto';
 import { Line } from "react-chartjs-2";
+import { useEffect, useState } from 'react';
 
 function StockChart() {
+    const [dataPoints , setDataPoints] = useState<{x: Date, y: number}[]>([]);
+
+    useEffect(()=>{
+        setDataPoints([
+            {
+                x: new Date('2016-12-25'),
+                y: 20
+            },
+            {
+                x: new Date('2016-12-26 8:00:00'),
+                y: 11
+            },
+            {
+                x: new Date('2016-12-26 10:00:00'),
+                y: 12
+            },
+            {
+                x: new Date('2016-12-26 11:00:00'),
+                y: 9
+            },
+            {
+                x: new Date('2016-12-26 12:00:00'),
+                y: 12
+            },
+            {
+                x: new Date('2016-12-30'),
+                y: 10
+            }]);
+    }, []);
+
     const data: ChartData<"line", { x: Date, y: number }[]> = {
         datasets: [{
             backgroundColor: '#067194',
             borderColor: '#067194',
-            data: [
-                {
-                    x: new Date('2016-12-25'),
-                    y: 20
-                },
-                {
-                    x: new Date('2016-12-26 8:00:00'),
-                    y: 11
-                },
-                {
-                    x: new Date('2016-12-26 10:00:00'),
-                    y: 12
-                },
-                {
-                    x: new Date('2016-12-26 11:00:00'),
-                    y: 9
-                },
-                {
-                    x: new Date('2016-12-26 12:00:00'),
-                    y: 12
-                },
-                {
-                    x: new Date('2016-12-30'),
-                    y: 10
-                }],
+            data: dataPoints,
         }]
     };
 
